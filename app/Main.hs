de642545dd215b5d828595f7cfd2b50d76ec8cd7
Etienne Batise <etienne.batise@gmail.com>
2019-04-11 18:18:52 +0200

    Add simple REPL
    Implements [part 1](https://cstack.github.io/db_tutorial/parts/part1.html)

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,41 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
+import Control.Monad
+import Data.String.Conversions
+import Data.Text
+import qualified Data.Text.Format as Fmt
 import Lib
+import Text.Parsec (parse)
+import Text.Parsec.Char (string)
+import Text.Parsec.Error
+import Text.Parsec.Text (Parser)
+import System.Exit
+import System.IO
+
+
+cmdParser :: Text -> Parser Text
+cmdParser t = convertString <$> string (convertString t)
+
+exitParser :: Parser Text
+exitParser = cmdParser ".exit"
+
+parse' :: Parser a -> Text -> Either ParseError a
+parse' p i = parse p "" i
 
 main :: IO ()
-main = someFunc
+main =
+  forever $ do
+    printPrompt
+    input <- convertString <$> getLine :: IO Text
+    case parse exitParser "" input of
+      Left e -> do
+        Fmt.print "Unrecognized command '{}'\n" (Fmt.Only input)
+        main
+      Right p -> exitSuccess
+
+printPrompt :: IO ()
+printPrompt = do
+  putStr "db > "
+  hFlush stdout
